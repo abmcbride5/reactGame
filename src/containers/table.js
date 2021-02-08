af9a2da2c542f4007288cfb4f126333003a5ced7
abmcbride1 <abmcbride@jasperhelps.email>
2021-02-07 21:40:07 -0800

    adding clue for word

--- src/containers/table.js
+++ src/containers/table.js
@@ -26,6 +26,7 @@ class Table extends React.Component{
     render(){
         return (
             <div className="table">
+                <h2>Clue: food you eat</h2>
                 {this.createTable()}
             </div>
         )
